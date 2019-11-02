1645a4c529693b35ad3e06b928f840272b62bf00
breneswr <56894975+breneswr@users.noreply.github.com>
2019-11-01 21:12:32 -0500

    Update PercentChange.R

--- PercentChange.R
+++ PercentChange.R
@@ -1,5 +1,5 @@
 setwd("~/Documents/Mudac2019")
-
+rm(list = ls())
 SoybeanJuly<- read_excel("July.xlsx",
                          col_types = c("date", "text", "text", "text", "text"))
 SoybeanMarch <- read_excel("March.xlsx", 
@@ -14,11 +14,6 @@ SoybeanJuly.18 <-SoybeanJuly %>%
   filter(Year == 2018) %>%
   select(c(1:3,7)) %>%
   mutate(Contract = "July")
-
-
-
-
-
 
 SoybeanJuly.19 <-SoybeanJuly %>% 
   filter(Year == 2019) %>%
@@ -127,6 +122,95 @@ View(SoybeanMay)
 
 #take 9-2017 12-2017 
 #Find the percent decrease from 17 to 18
+
+#########################################################
+SoybeanJuly.17 <-SoybeanJuly %>% 
+  filter(Year == 2017) %>%
+  select(c(1:3,7)) %>%
+  mutate(Contract = "July")
+
+SoybeanJuly.18 <-SoybeanJuly %>% 
+  filter(Year == 2018) %>%
+  select(c(1:3,7)) %>%
+  mutate(Contract = "July")
+
+
+
+SoybeanJuly17.End<- 
+  SoybeanJuly.17 %>%
+  filter(Month %in% c("09", "10","11","12"))
+
+SoybeanJuly18.End<- 
+  SoybeanJuly.18 %>%
+  filter(Month %in% c("09", "10","11","12"))
+
+         
+July17.18 <-SoybeanJuly18.End %>% 
+  inner_join(SoybeanJuly17.End, by = c("Month" = "Month", "Day" = "Day"))
+
+
+July17.18<-
+  July17.18 %>%
+  select(-c(1,6,8))
+
+names(July17.18)[3:5] <- c("Close 2018", "Contract","Close 2017")
+
+July17.18<-
+  July17.18 %>%
+  select(1:2,4,5,3)
+  
+
+
+
+July17.18.Decrease<-
+July17.18%>%
+  mutate(Percent = (as.double(`Close 2018`) - as.double(`Close 2017`))/ as.double((`Close 2017`)))
+
+
+####figure out how to include decimal
+July17.19.Final.Value<-
+July17.18.Decrease%>%
+  mutate(July19.Final = ( as.double(`Close 2018`) - as.double(`Close 2018`) * -Percent))
+
+
+#Take July 2019 and add final months 
+SoybeanJuly.19 <-SoybeanJuly %>% 
+  filter(Year == 2019) %>%
+  select(c(1:3,7)) %>%
+  mutate(Contract = "July") %>%
+  mutate(Close = as.character(Close))
+
+
+
+
+July17.19.Final.Value<-
+July17.19.Final.Value %>%
+  mutate(Year = "2019") %>%
+  select(-c(4:6)) 
+  
+names(July17.19.Final.Value)[4]<- "Close"
+
+#reorganizing columns
+July17.19.Final.Value<-
+  July17.19.Final.Value%>%
+  select(5,1,2,4,3)
+
+July17.19.Final.Value<- 
+  July17.19.Final.Value%>%
+  mutate(Close = as.numeric(Close))
+
+EndResult.July<-
+SoybeanJuly.19 %>%
+  mutate(Close = as.numeric(Close)) %>%
+  union(July17.19.Final.Value)
+
+EndResult.July %>%
+  #filter(Year == '2019') %>%
+  ggplot(aes(x = Month, y = Close, color = Month)) +
+  ylim(850,1000) +
+  geom_point() 
+
+
 
 
 
